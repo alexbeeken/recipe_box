99574fe994d96bb8488fdd12278f43d8097a6e3f
Alex Beeken & Kathryn Carr <student@epicodus.com>
2015-01-29 12:11:44 -0800

    Add web functionality to add ingredients and recipes

--- app.rb
+++ app.rb
@@ -1,1 +1,52 @@
+require("bundler/setup")
+Bundler.require(:default)
+also_reload("/lib/**/*.rb")
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+
+get '/' do
+  @recipes = Recipe.all()
+  @ingredients = Ingredient.all()
+  erb(:index)
+end
+
+get '/recipe/:id' do
+  @recipe = Recipe.find(params.fetch("id").to_i)
+  @ingredients = @recipe.ingredients
+  erb(:recipe)
+end
+
+get '/ingredient/:id' do
+  erb(:ingredient)
+end
+
+get '/new_recipe' do
+  @edit = false
+  @ingredients = Ingredient.all()
+  erb(:recipe_form)
+end
+
+post '/recipe_add' do
+  title = params.fetch("title")
+  instructions = params.fetch("instructions")
+  ingredient_ids = params.fetch("ingredients")
+  ingredients = []
+  ingredient_ids.each() do |id|
+    ingredients.push(Ingredient.find(id.to_i()))
+  end
+  recipe = Recipe.create({:title => title, :instructions => instructions})
+  recipe.ingredients(ingredients)
+  redirect '/'
+end
+
+post '/add_ingredient' do
+  name = params.fetch("name")
+  Ingredient.create({:name => name})
+  redirect('/')
+end
+
+post '/add_ingredient_recipe_form' do
+  name = params.fetch("name")
+  Ingredient.create({:name => name})
+  redirect('/new_recipe')
+end
